b4ee2f19826634a2c815c247adf3df7501cc08ce
Marschi47 <florian.marschalek@gmail.com>
2026-03-29 22:03:00 +0200

    Queue instead of List for CPS Tracker

--- src/main/java/com/github/marschi47/basichud/CpsTracker.java
+++ src/main/java/com/github/marschi47/basichud/CpsTracker.java
@@ -2,24 +2,23 @@ package com.github.marschi47.basichud;
 
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class CpsTracker {
 
     // final cps count
     public static int leftCps = 0;
     public static int rightCps = 0;
-    private static final List<Long> rCLICKS = new ArrayList<>();
-    private static final List<Long> lCLICKS = new ArrayList<>();
+    private static final Queue<Long> rCLICKS = new ArrayDeque<>();
+    private static final Queue<Long> lCLICKS = new ArrayDeque<>();
 
     @SubscribeEvent
     public void onClientTick(TickEvent.ClientTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
             long currentTime = System.currentTimeMillis();
 
-            // if click add to list
+            // if click add to queue
             if (MouseInputHandler.leftClickedThisTick) {
                 lCLICKS.add(currentTime);
                 MouseInputHandler.leftClickedThisTick = false; // reset flag
@@ -30,12 +29,12 @@ public class CpsTracker {
             }
 
             // older than 1 sec gets removed
-            while (!lCLICKS.isEmpty() && lCLICKS.get(0) < currentTime - 1000) {
-                lCLICKS.remove(0);
+            while (!lCLICKS.isEmpty() && lCLICKS.peek() < currentTime - 1000) {
+                lCLICKS.poll();
             }
 
-            while (!rCLICKS.isEmpty() && rCLICKS.get(0) < currentTime - 1000) {
-                rCLICKS.remove(0);
+            while (!rCLICKS.isEmpty() && rCLICKS.peek() < currentTime - 1000) {
+                rCLICKS.poll();
             }
 
             // set final count
