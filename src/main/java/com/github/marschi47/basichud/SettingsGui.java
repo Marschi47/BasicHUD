0b44669e00a7fca9b2fc7a5095e2a38dc4444549
Marschi47 <florian.marschalek@gmail.com>
2026-03-30 21:23:31 +0200

    color fields with preview in the new config menu

--- src/main/java/com/github/marschi47/basichud/SettingsGui.java
+++ src/main/java/com/github/marschi47/basichud/SettingsGui.java
@@ -2,10 +2,20 @@ package com.github.marschi47.basichud;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.gui.Gui;
 import java.io.IOException;
 
 public class SettingsGui extends GuiScreen {
     private final GuiScreen parentScreen;
+    private boolean skipSaveOnClose = false;
+
+    private GuiTextField fpsColorField;
+    private GuiTextField pingColorField;
+    private GuiTextField cpsColorField;
+    private GuiTextField keystrokesColorField;
+    private GuiTextField keystrokesActivatedColorField;
+    private GuiTextField potionHudColorField;
 
     public SettingsGui(GuiScreen parentScreen) {
         this.parentScreen = parentScreen;
@@ -28,6 +38,7 @@ public class SettingsGui extends GuiScreen {
                 .add(new GuiToggleButton(12, col1, y + gap, btnW, btnH, "FPS Background", MyModConfig.fpsBackground));
         this.buttonList
                 .add(new GuiToggleButton(13, col1, y + gap * 2, btnW, btnH, "FPS Chroma", MyModConfig.fpsChroma));
+        fpsColorField = createColorField(col1, y + gap * 3, btnW, btnH, MyModConfig.fpsColor);
 
         // Ping (right column)
         this.buttonList.add(new GuiToggleButton(2, col2, y, btnW, btnH, "Show Ping", MyModConfig.pingEnabled));
@@ -35,14 +46,16 @@ public class SettingsGui extends GuiScreen {
                 .add(new GuiToggleButton(22, col2, y + gap, btnW, btnH, "Ping Background", MyModConfig.pingBackground));
         this.buttonList
                 .add(new GuiToggleButton(23, col2, y + gap * 2, btnW, btnH, "Ping Chroma", MyModConfig.pingChroma));
+        pingColorField = createColorField(col2, y + gap * 3, btnW, btnH, MyModConfig.pingColor);
 
         // CPS (left column, second block)
-        int y2 = y + gap * 4;
+        int y2 = y + gap * 5;
         this.buttonList.add(new GuiToggleButton(3, col1, y2, btnW, btnH, "Show CPS", MyModConfig.cpsEnabled));
         this.buttonList
                 .add(new GuiToggleButton(32, col1, y2 + gap, btnW, btnH, "CPS Background", MyModConfig.cpsBackground));
         this.buttonList
                 .add(new GuiToggleButton(33, col1, y2 + gap * 2, btnW, btnH, "CPS Chroma", MyModConfig.cpsChroma));
+        cpsColorField = createColorField(col1, y2 + gap * 3, btnW, btnH, MyModConfig.cpsColor);
 
         // Keystrokes (right column, second block)
         this.buttonList
@@ -51,9 +64,12 @@ public class SettingsGui extends GuiScreen {
                 MyModConfig.keystrokesBackground));
         this.buttonList.add(new GuiToggleButton(43, col2, y2 + gap * 2, btnW, btnH, "Keystrokes Chroma",
                 MyModConfig.keystrokesChroma));
+        keystrokesColorField = createColorField(col2, y2 + gap * 3, btnW, btnH, MyModConfig.keystrokesColor);
+        keystrokesActivatedColorField = createColorField(col2, y2 + gap * 4, btnW, btnH,
+                MyModConfig.keystrokesActivatedColor);
 
         // Potion HUD (centered, third block)
-        int y3 = y2 + gap * 4;
+        int y3 = y2 + gap * 6;
         int potCenterX = centerX - 100;
         this.buttonList.add(
                 new GuiToggleButton(5, potCenterX, y3, btnW, btnH, "Show Potion HUD", MyModConfig.potionHudEnabled));
@@ -61,10 +77,19 @@ public class SettingsGui extends GuiScreen {
                 MyModConfig.potionHudBackground));
         this.buttonList.add(new GuiToggleButton(53, potCenterX, y3 + gap * 2, btnW, btnH, "Potion HUD Chroma",
                 MyModConfig.potionHudChroma));
+        potionHudColorField = createColorField(potCenterX, y3 + gap * 3, btnW, btnH, MyModConfig.potionHudColor);
 
         // Bottom Buttons
         this.buttonList.add(new GuiButton(100, centerX - 205, this.height - 30, 200, 20, "Back to Position Editor"));
         this.buttonList.add(new GuiButton(101, centerX + 5, this.height - 30, 200, 20, "\u00a7cReset to Default"));
+    }
+
+    private GuiTextField createColorField(int x, int y, int width, int height, String currentValue) {
+        GuiTextField field = new GuiTextField(0, mc.fontRendererObj, x + 20, y, width - 20, height);
+        field.setMaxStringLength(6);
+        field.setText(currentValue);
+        field.setValidator(input -> input != null && input.matches("[0-9a-fA-F]*"));
+        return field;
     }
 
     @Override
@@ -72,20 +97,141 @@ public class SettingsGui extends GuiScreen {
         this.drawDefaultBackground();
         this.drawCenteredString(mc.fontRendererObj, "BasicHUD Settings", this.width / 2, 15, 0xFFFFFF);
 
-        // Section labels
-        int col1 = this.width / 2 - 205;
-        int col2 = this.width / 2 + 5;
+        int centerX = this.width / 2;
+        int col1 = centerX - 205;
+        int col2 = centerX + 5;
+        int gap = 24;
         int y1Label = 30;
-        int y2Label = 30 + 24 * 4;
-        int y3Label = y2Label + 24 * 4;
+        int y2Label = 30 + gap * 5;
+        int y3Label = y2Label + gap * 6;
 
         mc.fontRendererObj.drawStringWithShadow("FPS", col1, y1Label, 0xFFFFFF);
         mc.fontRendererObj.drawStringWithShadow("Ping", col2, y1Label, 0xFFFFFF);
         mc.fontRendererObj.drawStringWithShadow("CPS", col1, y2Label, 0xFFFFFF);
         mc.fontRendererObj.drawStringWithShadow("Keystrokes", col2, y2Label, 0xFFFFFF);
-        mc.fontRendererObj.drawStringWithShadow("Potion HUD", this.width / 2 - 40, y3Label, 0xF3F3F3);
+        mc.fontRendererObj.drawStringWithShadow("Potion HUD", centerX - 40, y3Label, 0xF3F3F3);
+
+        // Color field labels (# prefix)
+        int y = 40;
+        drawColorPreview(col1, y + gap * 3, fpsColorField, null);
+        drawColorPreview(col2, y + gap * 3, pingColorField, null);
+
+        int y2 = y + gap * 5;
+        drawColorPreview(col1, y2 + gap * 3, cpsColorField, null);
+        drawColorPreview(col2, y2 + gap * 3, keystrokesColorField, null);
+        drawColorPreview(col2, y2 + gap * 4, keystrokesActivatedColorField, "Active");
+
+        int y3 = y2 + gap * 6;
+        int potCenterX = centerX - 100;
+        drawColorPreview(potCenterX, y3 + gap * 3, potionHudColorField, null);
+
+        fpsColorField.drawTextBox();
+        pingColorField.drawTextBox();
+        cpsColorField.drawTextBox();
+        keystrokesColorField.drawTextBox();
+        keystrokesActivatedColorField.drawTextBox();
+        potionHudColorField.drawTextBox();
 
         super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    private void drawColorPreview(int x, int y, GuiTextField field, String label) {
+        String hex = field.getText();
+        int previewColor = 0xFFFFFF;
+        if (hex.length() == 6) {
+            try {
+                previewColor = Integer.parseInt(hex, 16);
+            } catch (NumberFormatException ignored) {
+            }
+        }
+        int size = 16;
+        int px = x + 1;
+        int py = y + 2;
+        Gui.drawRect(px - 1, py - 1, px + size + 1, py + size + 1, 0xFF000000);
+        Gui.drawRect(px, py, px + size, py + size, 0xFF000000 | previewColor);
+        if (label != null) {
+            mc.fontRendererObj.drawStringWithShadow("\u00a77" + label, px + size + 4, y + 6, 0xAAAAAA);
+        }
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        fpsColorField.textboxKeyTyped(typedChar, keyCode);
+        pingColorField.textboxKeyTyped(typedChar, keyCode);
+        cpsColorField.textboxKeyTyped(typedChar, keyCode);
+        keystrokesColorField.textboxKeyTyped(typedChar, keyCode);
+        keystrokesActivatedColorField.textboxKeyTyped(typedChar, keyCode);
+        potionHudColorField.textboxKeyTyped(typedChar, keyCode);
+        super.keyTyped(typedChar, keyCode);
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        fpsColorField.mouseClicked(mouseX, mouseY, mouseButton);
+        pingColorField.mouseClicked(mouseX, mouseY, mouseButton);
+        cpsColorField.mouseClicked(mouseX, mouseY, mouseButton);
+        keystrokesColorField.mouseClicked(mouseX, mouseY, mouseButton);
+        keystrokesActivatedColorField.mouseClicked(mouseX, mouseY, mouseButton);
+        potionHudColorField.mouseClicked(mouseX, mouseY, mouseButton);
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
+    @Override
+    public void updateScreen() {
+        super.updateScreen();
+        fpsColorField.updateCursorCounter();
+        pingColorField.updateCursorCounter();
+        cpsColorField.updateCursorCounter();
+        keystrokesColorField.updateCursorCounter();
+        keystrokesActivatedColorField.updateCursorCounter();
+        potionHudColorField.updateCursorCounter();
+    }
+
+    @Override
+    public void onGuiClosed() {
+        if (!skipSaveOnClose) {
+            saveAllColors();
+        }
+        super.onGuiClosed();
+    }
+
+    private void saveAllColors() {
+        saveColor("HUD_FPS", "fpsColor", fpsColorField.getText(), MyModConfig.fpsColor, (val) -> {
+            MyModConfig.fpsColor = val;
+            MyModConfig.fpsColorInt = Integer.parseInt(val, 16);
+        });
+        saveColor("HUD_PING", "pingColor", pingColorField.getText(), MyModConfig.pingColor, (val) -> {
+            MyModConfig.pingColor = val;
+            MyModConfig.pingColorInt = Integer.parseInt(val, 16);
+        });
+        saveColor("HUD_CPS", "cpsColor", cpsColorField.getText(), MyModConfig.cpsColor, (val) -> {
+            MyModConfig.cpsColor = val;
+            MyModConfig.cpsColorInt = Integer.parseInt(val, 16);
+        });
+        saveColor("HUD_Keystrokes", "keystrokesColor", keystrokesColorField.getText(), MyModConfig.keystrokesColor,
+                (val) -> {
+                    MyModConfig.keystrokesColor = val;
+                    MyModConfig.keystrokesColorInt = Integer.parseInt(val, 16);
+                });
+        saveColor("HUD_Keystrokes", "keystrokesActivatedColor", keystrokesActivatedColorField.getText(),
+                MyModConfig.keystrokesActivatedColor, (val) -> {
+                    MyModConfig.keystrokesActivatedColor = val;
+                    MyModConfig.keystrokesActivatedColorInt = Integer.parseInt(val, 16);
+                });
+        saveColor("HUD_POTIONS", "potionHudColor", potionHudColorField.getText(), MyModConfig.potionHudColor, (val) -> {
+            MyModConfig.potionHudColor = val;
+            MyModConfig.potionHudColorInt = Integer.parseInt(val, 16);
+        });
+        MyModConfig.config.save();
+    }
+
+    private void saveColor(String category, String key, String newValue, String currentValue,
+            java.util.function.Consumer<String> applier) {
+        String upper = newValue.toUpperCase();
+        if (upper.length() == 6 && upper.matches("[0-9A-F]{6}") && !upper.equals(currentValue)) {
+            applier.accept(upper);
+            MyModConfig.config.get(category, key, "FFFFFF").set(upper);
+        }
     }
 
     @Override
@@ -170,6 +316,7 @@ public class SettingsGui extends GuiScreen {
         }
 
         if (button.id == 101) {
+            skipSaveOnClose = true;
             MyModConfig.resetToDefaults();
             mc.displayGuiScreen(new SettingsGui(parentScreen));
         }
